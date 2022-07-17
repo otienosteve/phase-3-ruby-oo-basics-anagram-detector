f346362062faa8ad7c76a1a4a6d364d120c2eba1
otienosteve <steveotieno701@gmail.com>
2022-07-17 19:58:13 +0300

    commit tree 4d0e976ea04d43c9291b6a52c56a9479ececb3ef

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+class Anagram
+    def initialize(word)
+    @word=word
+    end
+    def match(words)
+        @words=words
+        d=@words.map {|word| word.chars.sort.join}
+        zword= @word.chars.sort.join
+        arr=[]
+        d.each_with_index do|wrd,i|
+        if wrd==zword
+            arr.push(i)
+        end
+            
+        end
+        fnd=[]
+        arr.each do |idx|
+            fnd.push(@words[idx])
+        end
+
+        fnd
+    end
+    
+end
